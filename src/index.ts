f70c0bff61b7b9ee5e78802fc351ad015d7ce435
Ammar Ansari <iammaransari@gmail.com>
2023-05-09 19:36:18 +0200

    Integrate AI noun

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 const twilio = require('../lib')
-import { getHost, Reject } from './helpers'
+import { getHost, Reject, AI } from './helpers'
 import { Twilio, TwimlInterface, JwtInterface } from '../index'
 import { CompatibilityAPIRestClientOptions, RestClient as RC } from '../compatibility-api'
 
 twilio.twiml.FaxResponse.prototype.reject = function (attributes: any) {
   return new Reject(this.response.ele('Reject', attributes))
+}
+
+const connectRef = twilio.twiml.VoiceResponse.prototype.connect
+twilio.twiml.VoiceResponse.prototype.connect = function (attributes: any) {
+  const self = this;
+  const connect = connectRef.call(self, attributes)
+  connect.AI = () => {
+    return new AI(this.response.ele('AI', attributes), this.response);
+  }
+  return connect
 }
 
 /** @remarks See index.d.ts for types */
