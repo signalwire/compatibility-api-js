0910571b19319c1ba8454bc3f34ca87b0e15739a
Ammar Ansari <iammaransari@gmail.com>
2023-05-10 18:02:21 +0200

    Revert "Integrate AI noun"
    This reverts commit 06e341cae3082ef7785f732b950ac4f8fa3fa1a1.

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,91 +1,12 @@
 const twilio = require('../lib')
-import TwiML from '../lib/twiml/TwiML'
 
 function Reject(reject: any) {
   this.reject = reject
   this._propertyName = 'reject'
 }
+
 Reject.prototype = Object.create(twilio.twiml.FaxResponse.prototype)
 Reject.prototype.constructor = 'Reject'
-
-function Prompt(prompt) {
-  this.prompt = prompt;
-  this._propertyName = 'prompt';
-
-  this.setTemperature = (temperature) => {
-    return this.temperature = temperature
-  }
-
-  this.setTopP = (topP) => {
-    return this.topP = topP
-  }
-
-  this.setConfidence = (confidence) => {
-    return this.confidence = confidence
-  }
-
-  this.setBargeConfidence = (bargeConfidence) => {
-    return this.bargeConfidence = bargeConfidence
-  }
-
-  this.setPresencePenalty = (presencePenalty) => {
-    return this.presencePenalty = presencePenalty
-  }
-
-  this.setFrequencyPenalty = (frequencyPenalty) => {
-    return this.frequencyPenalty = frequencyPenalty
-  }
-}
-Prompt.prototype = Object.create(TwiML.prototype);
-Prompt.prototype.constructor = Prompt;
-
-// Creates a new PostPrompt instance that inherits from Prompt
-function PostPrompt(prompt) {
-  Prompt.call(this, prompt);
-  this._propertyName = 'postPrompt';
-}
-PostPrompt.prototype = Object.create(Prompt.prototype);
-PostPrompt.prototype.constructor = PostPrompt;
-
-function AI(ai: any, response) {
-  this.ai = ai;
-  this._propertyName = 'ai';
-  this.response = response;
-  
-  this.setEngine = (engine) => {
-    return this.engine = engine
-  }
-
-  this.setVoice = (voice) => {
-    return this.voice = voice
-  }
-
-  this.setPostPromptURL = (postPromptURL) => {
-    return this.postPromptURL = postPromptURL
-  }
-
-  this.setPostPromptAuthUser = (postPromptAuthUser) => {
-    return this.postPromptAuthUser = postPromptAuthUser
-  }
-
-  this.setPostPromptAuthPassword = (postPromptAuthPassword) => {
-    return this.postPromptAuthPassword = postPromptAuthPassword
-  }
-
-  this.setHints = (hints) => {
-    return this.hints = hints
-  }
-
-  this.prompt = (value) => {
-    return new Prompt(this.response.ele('prompt', value));
-  }
-
-  this.postPrompt = (value) => {
-    return new PostPrompt(this.response.ele('postPrompt', value));
-  }
-}
-AI.prototype = Object.create(TwiML.prototype);
-AI.prototype.constructor = AI;
 
 const getHost = (opts: { signalwireSpaceUrl?: string } = {}): string => {
   const { signalwireSpaceUrl } = opts
@@ -104,4 +25,4 @@ const getHost = (opts: { signalwireSpaceUrl?: string } = {}): string => {
   )
 }
 
-export { getHost, Reject, AI }
+export { getHost, Reject }
